005d8f0c7b7114711f4e367cda934803617b86cc
Richard Willis <willis.rh@gmail.com>
2020-06-09 18:55:39 +0100

    Use ShellExecution

--- src/extension.ts
+++ src/extension.ts
@@ -10,7 +10,7 @@ export function activate(context: vscode.ExtensionContext) {
 				vscode.TaskScope.Workspace,
 				"Test Task",
 				"tasktype",
-				new vscode.ProcessExecution("echo", ["hello"])
+				new vscode.ShellExecution("echo", ["hello"])
 			);
 			task.presentationOptions = {
 				showReuseMessage: false,
